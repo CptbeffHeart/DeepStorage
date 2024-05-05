b7e5c1985243cc81d3e3d7fca920491ab9c87f5b
CptBeffHeart <cptbeffheart@gmail.com>
2024-05-05 14:39:23 +0200

    feat: add DeepStorage.kt

---
+++ src/main/kotlin/com/expectale/DeepStorage.kt
@@ -0,0 +1,13 @@
+package com.expectale
+
+import xyz.xenondevs.nova.addon.Addon
+
+object DeepStorage : Addon() {
+    
+    override fun init() {}
+    
+    override fun onEnable() {}
+    
+    override fun onDisable() {}
+    
+}
