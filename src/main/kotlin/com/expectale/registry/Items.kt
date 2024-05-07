2fed4ee05b4ec37db3b3327fa1cb1fe6d6c7a427
CptBeffHeart <cptbeffheart@gmail.com>
2024-05-07 21:15:41 +0200

    feat: add 4k, 16k, 64k to Items

--- src/main/kotlin/com/expectale/registry/Items.kt
+++ src/main/kotlin/com/expectale/registry/Items.kt
@@ -11,6 +11,9 @@ object Items: ItemRegistry by DeepStorage.registry {
     
     //Storage cells
     val STORAGE_CELL_1k = registerItem("storage_cell_1k", StorageCell)
+    val STORAGE_CELL_4k = registerItem("storage_cell_4k", StorageCell)
+    val STORAGE_CELL_16k = registerItem("storage_cell_16k", StorageCell)
+    val STORAGE_CELL_64k = registerItem("storage_cell_64k", StorageCell)
     
     //Blocks
     val DEEP_STORAGE_UNIT = registerItem(Blocks.DEEP_STORAGE_UNIT)
