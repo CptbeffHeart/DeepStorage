d9fd764d2bfb3fc928bae34c3f5a1f4e29145ad6
CptBeffHeart <cptbeffheart@gmail.com>
2024-05-07 17:05:52 +0200

    feat: add Logger

--- src/main/kotlin/com/expectale/DeepStorage.kt
+++ src/main/kotlin/com/expectale/DeepStorage.kt
@@ -1,13 +1,14 @@
 package com.expectale
 
 import xyz.xenondevs.nova.addon.Addon
+import java.util.logging.Logger
+
+lateinit var LOGGER: Logger
 
 object DeepStorage : Addon() {
     
-    override fun init() {}
-    
-    override fun onEnable() {}
-    
-    override fun onDisable() {}
+    override fun init() {
+        LOGGER = logger
+    }
     
 }
