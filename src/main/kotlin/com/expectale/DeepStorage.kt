0ee4f51bdbf0c23d1bc404bbc6c2485a343724bf
CptbeffHeart <51067790+CptbeffHeart@users.noreply.github.com>
2024-06-22 17:44:14 +0200

    feat: add project distributor (#9)

--- src/main/kotlin/com/expectale/DeepStorage.kt
+++ src/main/kotlin/com/expectale/DeepStorage.kt
@@ -1,11 +1,14 @@
 package com.expectale
 
 import xyz.xenondevs.nova.addon.Addon
+import xyz.xenondevs.nova.update.ProjectDistributor
 import java.util.logging.Logger
 
 lateinit var LOGGER: Logger
 
 object DeepStorage : Addon() {
+    
+    override val projectDistributors = listOf(ProjectDistributor.github("CptbeffHeart/DeepStorage"))
     
     override fun init() {
         LOGGER = logger
